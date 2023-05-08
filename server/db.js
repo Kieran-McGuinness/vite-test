22bbc7f8ea08cb325e4737f8d5befd00c83afafa
Kieran <zumaht@gmail.com>
2023-05-08 15:05:47 -0400

    removed db password

--- server/db.js
+++ server/db.js
@@ -3,11 +3,7 @@ require("dotenv").config();
 const { DB_PASSWORD } = process.env;
 const pool = new Pool({
   user: "postgres",
-<<<<<<< HEAD
   password: `${DB_PASSWORD}`,
-=======
-  password: 
->>>>>>> adb9af9277a8ef30f17174b644a957e50bb37b3f
   host: "localhost",
   port: 5432,
   database: "todo",
